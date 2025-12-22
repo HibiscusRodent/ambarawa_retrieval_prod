e233d59feeac709e5b93c3b34384553126a2ff39
HibiscusRodent <adityoputomo@gmail.com>
2025-12-22 17:35:49 +0700

    changed some class names, imported all main types into the lance function

---
+++ semi_prod.py
@@ -0,0 +1,7 @@
+import lancedb
+from Pathlib import Path
+from pydantic_to_pyarrow import get_pyarrow_schema
+from image_processors import ImageProcessors
+
+from baml_client.sync_client import b
+from baml_client.types import BookConditionData, BookContentHints, BookMainData, BookPublisherAndDistributorDetails, RawAnalysis
