e8f17df0ef33510ac97868682e1fb2f3c0730c71
HibiscusRodent <adityoputomo@gmail.com>
2025-12-26 09:27:55 +0700

    inference as function runs

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import inferenceFunctions as inf
 import asyncio
+from prefect import task, flow
 
-book_folder_path = "sample_data\\rak-0018_baris-002_buku-12"
-inf.configure_environment()
 
 book_folder_path = "sample_data/rak-0018_baris-002_buku-12"
 output_folder_path = "data/output_book_data"
 lance_db_uri = "data/test/lance_db_semi_prod"
 table_name = "active_table_lots_columns"
 
-asyncio.run(
-    inf.process_one_book_flow(
-        book_folder_path=book_folder_path,
-        output_folder_path=output_folder_path,
-        lance_db_uri=lance_db_uri,
-        table_name=table_name,
+def run_single_inference(
+    book_folder_path: str,
+    output_folder_path: str,
+    lance_db_uri: str,
+    table_name: str,
+    ) -> None:
+    """
+    Run inference on a single book and store results in LanceDB.
+    """
+    
+    inf.configure_environment()
+    asyncio.run(
+        inf.process_one_book_flow(
+            book_folder_path=book_folder_path,
+            output_folder_path=output_folder_path,
+            lance_db_uri=lance_db_uri,
+            table_name=table_name,
+            )
+        )
+    return None
+
+run_single_inference(
+    book_folder_path=book_folder_path,
+    output_folder_path=output_folder_path,
+    lance_db_uri=lance_db_uri,
+    table_name=table_name,
     )
-)
+
+
+
+
