dc60caafccd18b308ac03b26189aae5f8014b3af
HibiscusRodent <adityoputomo@gmail.com>
2025-12-26 10:01:29 +0700

    added safeguards to the storage config

--- storage.config.py
+++ storage.config.py
@@ -1,3 +1,5 @@
+import __main__
+from dask.dot import name
 import lancedb
 import Pathlib
 import os
@@ -6,9 +8,22 @@ import os
 
 
 # ------ input files
-sample_data_paths = "./sample_data"
-sample_book_one = os.path.join(sample_data_paths, "rak-0018_baris-002_buku-12")
-sample_book_two = os.path.join(sample_data_paths, "rak-0003_baris-005_buku-30")
+def inputFiles():
+    sample_data_paths = "./sample_data"
+    sample_book_one = os.path.join(sample_data_paths, "rak-0018_baris-002_buku-12")
+    sample_book_two = os.path.join(sample_data_paths, "rak-0003_baris-005_buku-30")
+    return None
+    
+    
+    
+
 
 #--- output files
 output_data_path = "./data/output_book_data"
+
+
+def main() -> None:
+    return None
+
+if __name__ == "__main__":
+    main()
