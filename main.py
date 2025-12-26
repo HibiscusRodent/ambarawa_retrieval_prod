a55ffefe4666dc72512b083318ac8aa592353b7c
HibiscusRodent <adityoputomo@gmail.com>
2025-12-26 09:39:33 +0700

    running as no task works

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import asyncio
 from prefect import task, flow
 
 
-book_folder_path = "sample_data/rak-0018_baris-002_buku-12"
+book_folder_paths = ["sample_data/rak-0018_baris-002_buku-12", "sample_data/rak-0003_baris-005_buku-30"]
 output_folder_path = "data/output_book_data"
 lance_db_uri = "data/test/lance_db_semi_prod"
 table_name = "active_table_lots_columns"
+
 
 def run_single_inference(
     book_folder_path: str,
@@ -29,13 +30,28 @@ def run_single_inference(
         )
     return None
 
-run_single_inference(
-    book_folder_path=book_folder_path,
+def main_inference_multiple_books(
+    book_folder_paths: list[str],
+    output_folder_path: str,
+    lance_db_uri: str,
+    table_name: str,
+    ) -> None:
+    """
+    Run inference on multiple books and store results in LanceDB.
+    """
+    for book_folder_path in book_folder_paths:
+        run_single_inference(
+            book_folder_path=book_folder_path,
+            output_folder_path=output_folder_path,
+            lance_db_uri=lance_db_uri,
+            table_name=table_name,
+            )
+    return None
+
+main_inference_multiple_books(
+    book_folder_paths=book_folder_paths,
     output_folder_path=output_folder_path,
     lance_db_uri=lance_db_uri,
     table_name=table_name,
-    )
-
-
-
+)
 
