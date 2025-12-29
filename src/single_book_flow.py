acb29d261e3c0ca3eb2aac531e91c81e0c608af0
HibiscusRodent <adityoputomo@gmail.com>
2025-12-30 03:39:35 +0700

    added folder image processing

--- src/single_book_flow.py
+++ src/single_book_flow.py
@@ -1,6 +1,8 @@
 # ---- importing modules ----
 
-from image_processings import ImageProcessors
+from venv import logger
+from zipfile import Path
+from image_processings import ImageProcessors, ProcessedBookDict
 from baml_client.sync_client import b
 from baml_client.types import (
     BookConditionData,
@@ -18,24 +20,27 @@ from prefect.logging import get_run_logger
 #---- misc modules
 from dotenv import load_dotenv
 from rich.traceback import install
+from pathlib import Path
+
 
 @task
 def initiate_environment():
     env_keys_loader = load_dotenv()     # load dotenv variables for baml to access api key in the .env file
     rich_log_prettier = install()  # install rich traceback for better error logging
     logger = get_run_logger() # setting up logger for prefect
+    ip = ImageProcessors()  # initiate image processor to make sure all dependencies are loaded
     
     # print info logger to make sure that necessary environment variables are loaded
     logger.info(f"Loaded environment variables: {env_keys_loader}")
     logger.info("Environment configured and variables loaded.")
     return None
 
-@task
+# utility functions
+
 def setup_output_directory(output_path):
     output_path.mkdir(parents=True, exist_ok=True)
     pass
 
-@task
 def save_to_json(data, output_path):
     """
     Saving a Pydantic model to a JSON file on the disk, with proper formatting.
@@ -49,7 +54,16 @@ def save_to_json(data, output_path):
     """ 
     with output_path.open("w", encoding="utf-8") as f:
         f.write(data.model_dump_json(indent=4, ensure_ascii=False))
-# the flow that encapoolates all process within the book processing data
+        
+        
+# inference related tasks
+def process_image_folder(image_processor: ImageProcessors, folder_path: str):
+    data: ProcessedBookDict = image_processor.process_book_folder(Path(folder_path))
+    logger.info(f"Processed book with id of: {data.book_id}")
+    logger.info(f"Got a total of {len(data.binary_images)} images")
+    return data
+        
+# the flow that encapsulates all process within the book processing data
 @flow
 def single_book_flow():
     # intiate environment
